bb823d715113f708827754df43cf88a93256921f
Devanshu Prakash <devanshuprakash2301@gmail.com>
2025-09-28 18:36:43 +0530

    kk

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+export default function Hero(){
+    return(
+        <>
+            <h1>Coffee Traking for Coffee <abbr 
+            title="An enthusiast or devottee">
+                    Friends
+                </abbr>!
+            </h1>
+            <div className="benefits-list">
+                <h3 className="font-bolder">Try <span 
+                className="text-gradient">KaffeeMeter</span> and start ...
+                </h3>
+                <p>✅ Traking every coffee</p>
+                <p>✅ Measuring your blood caffeine levels</p>
+                <p>✅ Costing and quanitifying your addition</p>
+            </div>
+                      <div className="card info-card">
+                <div>
+                    <i className="fa-solid fa-circle-info"></i>
+                    <h3>Did you know...</h3>
+                </div>
+                <h5>That caffeine&apos;s half-life is about 5 hours?</h5>
+                <p>This means that after 5 hours, half 
+                    the caffeine you consumed is still in your system, 
+                    keeping you alert longer! So if you drink a cup of coffee with 200 mg of caffeine, 5 hours, later, you&apos;ll still have about 100 mg of 
+                    caffeine in your system.</p>
+            </div>
+
+
+      
+        </> 
+    )
+}
---
+++ src/components/Layout.jsx
@@ -0,0 +1,36 @@
+export default function Layout(props){
+    const {children} = props;
+    const header = (
+        <header>
+            <div>
+                <h1 className="text-gradient">KaffeeMeter</h1>
+                <p>For Coffee Insatiates</p>
+            </div>
+                <button>
+                    <p>Logout</p>
+                </button>
+         (
+                <button >
+                    <p>Sign up free</p>
+                    <i className="fa-solid fa-mug-hot"></i>
+                </button>
+            )
+        </header>
+    )
+
+    const footer = (
+        <footer>
+            <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://www.smoljames.com">Smoljames</a> <br />using the <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br />Check out the project on <a target="_black" href="https://www.github.com/jamezmca/reactjs-full-course">GitHub</a>!</p>
+        </footer>
+    )
+
+  return (
+    <>
+      {header}
+      <main>
+        {children}
+      </main>
+      {footer}
+    </>
+  ) 
+}
