764d09f23c81028228208209481d7564330b0c8b
Devanshu Prakash <devanshuprakash2301@gmail.com>
2025-09-29 09:59:48 +0530

    helo

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,23 @@
+function Statscard(props){
+    
+    return(
+        <div></div>
+    )
+}
+
+
+
+
+
 export default function Stats(){
     return(
         <>
         <div className="section-header">
             <i className="fa-solid fa-chart-simple" />
             <h2>Stats</h2>
+        </div>
+        <div className="stats-grid">
+
         </div>
         </> 
     )
